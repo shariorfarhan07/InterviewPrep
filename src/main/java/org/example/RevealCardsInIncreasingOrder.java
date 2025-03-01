d12f5317c6c9bfff5a87ec81b6f2d79f98687467
shariorfarhan07 <shariorfarhan77@gmail.com>
2025-03-01 12:43:09 +0600

    updated

---
+++ src/main/java/org/example/RevealCardsInIncreasingOrder.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RevealCardsInIncreasingOrder {
+    public static void main(String[] args) {
+        deckRevealedIncreasing(new int[]{1,2,3,4,5,6,7,8,9});
+    }
+    public static int[]  deckRevealedIncreasing(int[] deck) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> index = new LinkedList<Integer>();
+        for (int i = 0; i < deck.length; i++) {
+            q.add(i);
+        }
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            index.add(x);
+            if (q.isEmpty()) break;
+            q.add(q.poll());
+        }
+        Arrays.sort(deck);
+        int[] res = new int[deck.length];
+        for (int i = 0; i <deck.length ; i++) {
+            res[index.poll()] = deck[i];
+        }
+        System.out.println(Arrays.toString(res));
+        return res;
+    }
+}
---
+++ src/main/java/org/example/SearchinRotatedSortedArray.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SearchinRotatedSortedArray {
+    public static void main(String[] args) {
+        System.out.println("low "+search(new int[]{3,4,5,6,1,2},2));
+    }
+
+    public  static int search(int[] nums, int target) {
+        int low = searchSmallestBinarySearch(nums);
+        System.out.println("low elem"+low);
+        if (low == 0) { return  binarySearch(nums,target,low,nums.length-1); }
+        int val=binarySearch(nums,target,low,nums.length-1);
+        if (val==-1) return binarySearch(nums,target,0,low);
+        return val;
+    }
+
+    public static int binarySearch(int[] nums, int target, int low, int high) {
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+
+    }
+    public static int searchSmallestBinarySearch(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < nums[high]) {
+                high = mid;
+            }
+            else{
+                low = mid+1 ;
+            }
+
+        }
+
+        return low;
+    }
+
+
+}
